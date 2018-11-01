ea546eb5e6b298581a5f66ad943bedee2d38e57c
Keith Williams <keith@vikode.onmicrosoft.com>
2018-11-01 14:45:57 +0000

    Filling in the post slug automatically.

---
+++ DotNetCoreBlog/wwwroot/js/write-post-slug.js
@@ -0,0 +1,10 @@
+﻿var title = document.getElementById("Title");
+var slug = document.getElementById("UrlSlug");
+
+title.addEventListener("keyup", function (e) {
+    slug.value = title.value
+        .trim()
+        .replace(/[\W_]+/g, "-")
+        .toLowerCase();
+    return true;
+});
